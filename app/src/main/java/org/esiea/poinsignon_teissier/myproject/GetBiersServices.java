39b847ff21259a724b9bf17511c2502cdbc1d161
CTeissier <teissier@et.esiea.fr>
2016-11-08 09:48:02 +0100

    getbiers

---
+++ app/src/main/java/org/esiea/poinsignon_teissier/myproject/GetBiersServices.java
@@ -0,0 +1,61 @@
+package org.esiea.poinsignon_teissier.myproject;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class GetBiersServices extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_GET_ALL_BIERS = "org.esiea.poinsignon_teissier.myproject.action.GETALLBIERS";
+    private static final String TAG = "GetBiersServices";
+
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionGetAllBiers(Context context) {
+        Intent intent = new Intent(context, GetBiersServices.class);
+        intent.setAction(ACTION_GET_ALL_BIERS);
+        context.startService(intent);
+    }
+
+
+
+    public GetBiersServices() {
+        super("GetBiersServices");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_GET_ALL_BIERS.equals(action)) {
+                handleActionGetAllBiers();
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionGetAllBiers() {
+        // TODO: Handle action Foo
+        Log.v(TAG, "Thread service name:" + Thread.currentThread().getName());
+    }
+
+
+}
