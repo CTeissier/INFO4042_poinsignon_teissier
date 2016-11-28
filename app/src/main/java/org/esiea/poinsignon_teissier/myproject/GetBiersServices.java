52ae384749bd2d7b994a9010c1ba1028b6a07310
Dan <daniel.poinsignon@hotmail.fr>
2016-11-28 14:15:09 +0100

    Ajout RecyclerView, Drawer

--- app/src/main/java/org/esiea/poinsignon_teissier/myproject/GetBiersServices.java
+++ app/src/main/java/org/esiea/poinsignon_teissier/myproject/GetBiersServices.java
@@ -1,8 +1,8 @@
 package org.esiea.poinsignon_teissier.myproject;
 
 import android.app.IntentService;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
@@ -13,6 +13,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 /**
@@ -25,23 +26,16 @@ import java.net.URL;
 public class GetBiersServices extends IntentService {
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_GET_ALL_BIERS = "org.esiea.poinsignon_teissier.myproject.action.GETALLBIERS";
+    private static final String ACTION_GET_ALL_BIERS = "org.esiea.poinsignon_teissier.myproject.action.GET_ALL_BIERS";
     private static final String TAG = "GetBiersServices";
-    public static final String BIERS_UPDATE ="com.octip.cours.INF4042_11.BIERS_UPDATE";
+    public static final String BIERS_UPDATE = "com.octip.cours.INF4042_11.BIERS_UPDATE";
 
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
     // TODO: Customize helper method
-    public static void startActionGetAllBiers(Context context) {
+    public static void startActionGet_All_Biers(Context context) {
         Intent intent = new Intent(context, GetBiersServices.class);
         intent.setAction(ACTION_GET_ALL_BIERS);
         context.startService(intent);
     }
-
 
     public GetBiersServices() {
         super("GetBiersServices");
@@ -52,19 +46,34 @@ public class GetBiersServices extends IntentService {
         if (intent != null) {
             final String action = intent.getAction();
             if (ACTION_GET_ALL_BIERS.equals(action)) {
-                handleActionGetAllBiers();
+                handleActionGet_All_Biers();
             }
+        }
+
+    }
+
+    private void copyInputStreamToFile(InputStream in , File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in .read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            out.close(); in .close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionGetAllBiers() {
-        // TODO: Handle action Foo
-        Log.v(TAG, "Thread service name:" + Thread.currentThread().getName());
 
+
+
+
+
+    private void handleActionGet_All_Biers() {
+        // TODO: Handle action Get_All_Biers
+        Log.v(TAG, "Thread service name:" + Thread.currentThread().getName());
         URL url = null;
         try {
             url = new URL("http://binouze.fabrigli.fr/bieres.json");
@@ -76,30 +85,17 @@ public class GetBiersServices extends IntentService {
 
                 Log.d(TAG, "Bieres jon downloaded !");
                 LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(MainActivity.BIERS_UPDATE));
+
             }
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
-
+        } catch (ProtocolException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void copyInputStreamToFile(InputStream input, File file) {
 
-        try{
-            OutputStream out = new FileOutputStream(file);
-            byte[] buf = new byte[1024];
-            int len;
-            while((len=input.read(buf))>0){
-                out.write(buf,0,len);
-            }
-            out.close();
-            input.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
--- app/src/main/java/org/esiea/poinsignon_teissier/myproject/MainActivity.java
+++ app/src/main/java/org/esiea/poinsignon_teissier/myproject/MainActivity.java
@@ -1,46 +1,167 @@
 package org.esiea.poinsignon_teissier.myproject;
 
+import android.app.DatePickerDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.RecyclerView;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
 
     private static final String TAG = "GetBiersServices";
-    private RecyclerView rev_bieres=null;
-
+    private JSONArray json;
+    private RecyclerView rev_bieres = null;
+    private DatePickerDialog dpd = null;
+    private AlertDialog.Builder ad = null;
+    private AlertDialog alertDialog = null;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv_hw = (TextView)findViewById(R.id.tv_hello_world);
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        /*long date = System.currentTimeMillis();
-        SimpleDateFormat sdf = new SimpleDateFormat("MMM MM dd,yyyy h:mm a");
-        String dateString = sdf.format(date);*/
-        GetBiersServices.startActionGetAllBiers(this);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+
+        TextView btn_hw = (TextView) findViewById(R.id.btn_hello_world);
+        getString(R.string.hello_world);
+
+        GetBiersServices.startActionGet_All_Biers(this);
+
+        rev_bieres = (RecyclerView) findViewById(R.id.rev_biere);
+        rev_bieres.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        rev_bieres.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), rev_bieres, new ClickListener() {
+
+
+
+            @Override
+            public void onClick(View view, int position) {/*
+                if (position == 2 || position == 3 || position == 5 || position == 7 || position == 11 || position == 13 || position == 17 || position == 19 || position == 23 || position == 29 || position == 31 || position == 37 || position == 41 || position == 43 || position == 47 || position == 53 || position == 59 || position == 61 || position == 67 || position == 71 || position == 73 || position == 79 || position == 83 || position == 89 || position == 97) {
+                    Toast.makeText(MainActivity.this, "n°" + position + " " + getString(R.string.Unavailable), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "n°" + position + " " + getString(R.string.available), Toast.LENGTH_SHORT).show();
+                }
+
+*/
+            }
+
+
+            @Override
+            public void onLongClick(View view, final int position) {/*
+
+                if (position == 2 || position == 3 || position == 5 || position == 7 || position == 11 || position == 13 || position == 17 || position == 19 || position == 23 || position == 29 || position == 31 || position == 37 || position == 41 || position == 43 || position == 47 || position == 53 || position == 59 || position == 61 || position == 67 || position == 71 || position == 73 || position == 79 || position == 83 || position == 89 || position == 97) {
+                    Toast.makeText(MainActivity.this, "n°" + position + " " + getString(R.string.Unavailable), Toast.LENGTH_SHORT).show();
+                } else {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Validation")
+                            .setMessage(getString(R.string.addMessage))
+                            .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(MainActivity.this, "n°" + position + " " + getString(R.string.addedMessage), Toast.LENGTH_SHORT).show();
+
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(MainActivity.this, "" + getString(R.string.msg2), Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .show();
+
+                }*/
+            }
+
+
+
+
+        }));
+        json = getBiersFromFile();
+        rev_bieres.setAdapter(new BiersAdapter(json));
+
+
         IntentFilter intentFilter = new IntentFilter(BIERS_UPDATE);
-        LocalBroadcastManager.getInstance(this).registerReceiver(new BierUpdate(),intentFilter);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BierUpdate(), intentFilter);
+
+        ad = new AlertDialog.Builder(this);
+        btn_hw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.show();
+
+            }
+        });
+
+
+      /*  ad = new AlertDialog.Builder(this).setTitle("Validation").setMessage("Souhaitez-vous valider cette action ?").setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), getString(R.string.msg), Toast.LENGTH_LONG).show();
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), getString(R.string.msg2), Toast.LENGTH_LONG).show();
+            }
+        }).setIcon(android.R.drawable.ic_dialog_alert);*/
+
+
+
+        final FrameLayout frameView = new FrameLayout(this);
+        ad.setView(frameView);
+        alertDialog = ad.create();
+        LayoutInflater inflater = alertDialog.getLayoutInflater();
+        View dialoglayout = inflater.inflate(R.layout.dialog_view, frameView);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
+
 
     }
 
@@ -60,22 +181,75 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
             return true;
         }
 
+
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
         return super.onOptionsItemSelected(item);
     }
 
+
     public static final String BIERS_UPDATE = "com.octip.inf4042_11.BIERS_UPDATE";
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://org.esiea.poinsignon_teissier.myproject/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://org.esiea.poinsignon_teissier.myproject/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
+
+
     public class BierUpdate extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.d(TAG, getIntent().getAction());
+            // Mettre une notification ici
             rev_bieres.setAdapter(new BiersAdapter(getBiersFromFile()));
         }
     }
-    public JSONArray getBiersFromFile() {//enregistre un jsonarray avec le contenu du lien
+
+
+    public JSONArray getBiersFromFile() { //enregistre un jsonarray avec le contenu du lien
         try {
             InputStream is = new FileInputStream(getCacheDir() + "/" + "bieres.json");
             byte[] buffer = new byte[is.available()];
@@ -91,5 +265,108 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-}
+    private class BiersAdapter extends RecyclerView.Adapter < BiersAdapter.BierHolder > {
+        private JSONArray biers;
 
+        public BiersAdapter(JSONArray jsonarray) {
+            this.biers = jsonarray;
+        }
+
+        @Override
+        public BierHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+
+            LayoutInflater li = LayoutInflater.from(viewGroup.getContext());
+
+            View v = li.inflate(R.layout.rv_bier_element, viewGroup, false);
+
+            return new BierHolder(v);
+
+        }
+
+        @Override
+        public void onBindViewHolder(BierHolder bierHolder, int i) {
+
+            try {
+                JSONObject jObj = biers.getJSONObject(i);
+                String jS = jObj.getString("name");
+                bierHolder.name.setText(jS);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return biers.length();
+        }
+
+        class BierHolder extends RecyclerView.ViewHolder {
+
+            TextView name;
+
+
+            public BierHolder(View itemView) {
+                super(itemView);
+                name = (TextView) itemView.findViewById(R.id.rv_bier_element_name);
+            }
+        }
+    }
+    class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+
+        private GestureDetector gestureDetector;
+        private ClickListener clickListener;
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
+            Log.d("BLA", "constructor invoked");
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    Log.d("BLA", "onSimpleTapUp");
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+
+                        clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    }
+                    Log.d("BLA", "onLongPress");
+                    // super.onLongPress(e);
+                }
+            });
+
+        }
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            Log.d("BLA", "onInterceptTouchEvent" + gestureDetector.onTouchEvent(e) + " " + e);
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+            Log.d("BLA", "onTouchEvent" + e);
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+
+    public static interface ClickListener {
+        public void onClick(View view, int position);
+        public void onLongClick(View view, int position);
+    }
+}
