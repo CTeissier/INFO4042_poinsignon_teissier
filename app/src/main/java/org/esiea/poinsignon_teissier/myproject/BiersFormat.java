6c2f0db8270904408aeb25a51d9f4bd8d90896c1
Dan <daniel.poinsignon@hotmail.fr>
2016-11-28 14:24:01 +0100

    Ajout du README

---
+++ app/src/main/java/org/esiea/poinsignon_teissier/myproject/BiersFormat.java
@@ -0,0 +1,10 @@
+package org.esiea.poinsignon_teissier.myproject;
+
+/**
+ * Created by danie on 22/11/2016.
+ */
+
+public class BiersFormat {
+    int iconId;
+    String title;
+}
---
+++ app/src/main/java/org/esiea/poinsignon_teissier/myproject/Information.java
@@ -0,0 +1,7 @@
+package org.esiea.poinsignon_teissier.myproject;
+
+
+public class Information {
+    int iconId;
+    String title;
+}
